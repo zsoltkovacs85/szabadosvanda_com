fc48581fd7dd159bbebf51e522dc9ab71eb60e8b
Zsolt Kovacs <info@zsoltkovacs.co.uk>
2024-07-12 22:47:05 +0200

    Listing the gallery images of character design and illustration 4

--- resources/views/character_designs/show.blade.php
+++ resources/views/character_designs/show.blade.php
@@ -12,7 +12,6 @@
 
             @endforeach
         @endif
-
     </div>
 
 @endsection
--- resources/views/illustrations/show.blade.php
+++ resources/views/illustrations/show.blade.php
@@ -4,6 +4,14 @@
 
     <div class="row">
         <h3 class="text-center raleway-200 my-5 text-uppercase subtitle">Details</h3>
+        @if(isset($images) && count($images) > 0 )
+            @foreach($images as $image)
+                <div class="col-12 mb-5">
+                    <img src="{{env('ADMIN_URL')}}/{{$image->path}}" class="img-fluid">
+                </div>
+
+            @endforeach
+        @endif
     </div>
 
 @endsection
