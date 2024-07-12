56cc590ec2cea546798b1a6f421cb8356fb83320
Zsolt Kovacs <info@zsoltkovacs.co.uk>
2024-07-12 16:59:39 +0200

    Improving the text on pages

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
     <div class="row">
-        <h3 class="text-center raleway-200 my-5 text-uppercase subtitle">{{$page->title}}</h3>
-        <div class="mb-5">
+        <h3 class="raleway-200 my-5 text-uppercase subtitle">{{$page->title}}</h3>
+        <div class="mb-5 raleway-200">
             {!! $page->desc !!}
         </div>
     </div>
