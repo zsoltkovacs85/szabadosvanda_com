a21aff67e4ab935d1a188e6715e2fa5cf06a45f8
Zsolt Kovacs <info@zsoltkovacs.co.uk>
2024-07-12 14:42:53 +0200

    Adding 404 and coming soon pages 2

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -3,10 +3,7 @@
 @section('content')
 
     <div class="row">
-        <h3 class="text-center raleway-200 my-5 text-uppercase subtitle">Coming Soon</h3>
-        <div class="mb-5">
-
-        </div>
+        <h3 class="text-center raleway-200 mt-5 text-uppercase subtitle display-5">Coming Soon</h3>
     </div>
 
 @endsection
