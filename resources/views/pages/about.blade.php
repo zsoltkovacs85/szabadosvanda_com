ccce4c6f468e8b3ecd51e294eab696228b25bbc1
Zsolt Kovacs <info@zsoltkovacs.co.uk>
2024-08-18 14:57:00 +0200

    Added resume to About

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,31 +1,164 @@
 @extends('layouts.index')
 
 @section('content')
-    <div class="row">
-        <h3 class="raleway-200 my-5 text-uppercase subtitle text-center">About us</h3>
-        <div class="col-12 mb-5 raleway-200">
+    <div class="raleway-200">
+        <div class="row">
+            <h3 class="my-5 raleway-200 text-uppercase subtitle text-center">About us</h3>
+            <div class="col-12 mb-5">
 
-            <p>I am a self-taught 2D/3D artist from Hungary, with a passionfor both traditional and digital art. I
-                earned a master's degreein Computer Science, travelled worldwide, and moved toEngland to further improve
-                my English. When I finally had the opportunity, I quit my web developer job to follow mypassion, which
-                has been present since childhood. I am a strongcommunicator with an outstanding work ethic and the
-                abilityto meet deadlines consistently. Successful in working as part of a team and skilled at working
-                independently. Currentlyfocusing on developing core art skills and technical abilities, and working hard
-                on making a career switch to the Animationindustry as a character designer.
-            </p>
+                <p>I am a self-taught 2D/3D artist from Hungary, with a passionfor both traditional and digital art. I
+                    earned a master's degreein Computer Science, travelled worldwide, and moved toEngland to further
+                    improve
+                    my English. When I finally had the opportunity, I quit my web developer job to follow mypassion,
+                    which
+                    has been present since childhood. I am a strongcommunicator with an outstanding work ethic and the
+                    abilityto meet deadlines consistently. Successful in working as part of a team and skilled at
+                    working
+                    independently. Currentlyfocusing on developing core art skills and technical abilities, and working
+                    hard
+                    on making a career switch to the Animationindustry as a character designer.
+                </p>
 
-            <p>
-                I spent one year gaining experience in working under direct supervision with Senior Artist Rafael
-                D.Zuban ( <a href="www.rafilm.hu">www.rafilm.hu</a>) to meet deadlines and quality standards, building 3d scenes in Blender with
-                Subdivision Modelingtechnique and using Photoshop/Affinity Photo for the post-production of the final
-                piece. However, my real passion is character creation, which is why I continued my studies withart
-                fundamentals and 2D character design. I am present on twoonline course platforms, Schoolism and
-                Domestika to furthermy skills. Furthermore I completed an anatomy course onCGMA with Rey Bustos,
-                completed a few weeks mentoring with Wouter Tulp and currently I am being led by and receiving feedback
-                on my 2D studies from Vagner Farias, animation director
-            </p>
-            <h4 class="mt-5">SKILLS</h4>
+                <p>
+                    I spent one year gaining experience in working under direct supervision with Senior Artist Rafael
+                    D.Zuban ( <a href="www.rafilm.hu"><b>www.rafilm.hu</b></a>) to meet deadlines and quality standards,
+                    building
+                    3d scenes in Blender with
+                    Subdivision Modelingtechnique and using Photoshop/Affinity Photo for the post-production of the
+                    final
+                    piece. However, my real passion is character creation, which is why I continued my studies withart
+                    fundamentals and 2D character design. I am present on twoonline course platforms, Schoolism and
+                    Domestika to furthermy skills. Furthermore I completed an anatomy course onCGMA with Rey Bustos,
+                    completed a few weeks mentoring with Wouter Tulp and currently I am being led by and receiving
+                    feedback
+                    on my 2D studies from Vagner Farias, animation director
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <h4 class="">SKILLS</h4>
+            <div class="col-12 col-sm-6">
+                <h6 class="mt-5">2D:</h6>
+                <ul>
+                    <li>Concept design</li>
+                    <li>Art fundamentals</li>
+                    <li>Good understanding of visual language</li>
+                    <li>Knowledge of anatomy</li>
+                    <li>Visual storytelling skills</li>
+                    <li>Knowledge of the animation and game development process</li>
+                </ul>
+            </div>
+            <div class="col-12 col-sm-6">
+                <h6 class="mt-5">3D:</h6>
+                <ul>
+                    <li>Subdivision modeling</li>
+                    <li>Shading</li>
+                    <li>Texturing</li>
+                    <li>Lighting</li>
+                    <li>Still Renders</li>
+                    <li>Good communication skills</li>
+                    <li>Reliable and loyal</li>
+                    <li>Solution oriented</li>
+                    <li>Open-minded</li>
+                    <li>Intuitive</li>
+                    <li>Passionate about thinking outside the box</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <h4 class="mb-3">TECHNICAL KNOWLEDGE</h4>
+            <div class="col-12 col-sm-6">
+                <ul>
+                    <li>Procreate</li>
+                    <li>Adobe Photoshop ,Adobe Illustrator ,Adobe Indesign</li>
+                    <li>Blender</li>
+                    <li>Affinity photo</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>SASS</li>
+                    <li>Bootstrap</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <h3 class="my-5 ral/eway-200 text-uppercase subtitle text-left">LATEST WORK EXPERIENCES</h3>
+            <div class="col-12 mb-5">
+                <p><b>February 2023 – present:</b> Floor Manager / Supervisor – Ember Home – Royal Leamington Spa
+                </p>
+                <p>
+                    I am responsible for the daily operation of a family business while I continue my studies in my free
+                    time.
+                </p>
+
+
+                <p>
+                    <b>June 2020 – December 2022:</b>
+                </p>
+                <p>
+                    I left my web-developer job and invested my savings into stay at home
+                    focusing on self-studying 3D modeling in Blender (1 year) and continued my studies with 2D, art
+                    fundamentals and character design.
+                </p>
+
+
+                <p>
+                    <b>June 2017 –June 2020:</b> Front-End Developer - Volcanic - Manchester
+                </p>
+                <p> Successfully built and maintained responsive websites in the recruitment industry.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <h3 class="my-5 ral/eway-200 text-uppercase subtitle text-left">EDUCATION</h3>
+            <div class="col-12 mb-5">
+                <p>
+                    Computer Science Engineering Masters Degree, University of Miskolc, Hungary, 2009
+                </p>
+                <p>
+                    Graphics-DTP course, Top School Training Centre, Budapest, Hungary, 2010
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <h3 class="my-5 ral/eway-200 text-uppercase subtitle text-left">ADDITIONAL EDUCATION</h3>
+            <div class="col-12">
+                <h6 class="mt-5">Mentoring:</h6>
+                <ul>
+                    <li>Vagner Farias (Animation Director at Pinguim Contect)</li>
+                    <li>Daniel Schier (Character Designer at Warner Brothers)</li>
+                    <li>Wouter Tulp (Character Design, Visual Development)</li>
+                    <li>Rafael D.Zubán(www.rafilm.hu, Senior Artist )</li>
+
+                </ul>
+
+
+                <h6 class="mt-5">Online Courses:</h6>
+                <ul>
+                    <li>Drawing Basics(Stan Prokopenko)</li>
+                    <li>Figure Drawing Fundamentals(Stan Prokopenko)</li>
+                    <li>Characters for Animated Film(Daniel Arriaga, Schoolism)</li>
+                    <li>Fundamentals of Expressive Character Design(Wouter Tulp, Schoolism)</li>
+                    <li>Conceptual Characters(Wouter Tulp, Schoolism)</li>
+                    <li>Drawing Fundamentals(Thomas Fluharty, Schoolism)</li>
+                    <li>Deconstructed:Drawing People( Viktor Kalvachev, Schoolism)</li>
+                    <li>Digital Character Design: Draw Protagonists with Personality(Joel Santana, Domestika)</li>
+                    <li>Intro to Production Modeling, CG Master Academy ( JonathanLin, CGMA)</li>
+                    <li>Analytical Figure Drawing (Rey Bustos, CGMA)</li>
+
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <h3 class="my-5 ral/eway-200 text-uppercase subtitle text-left">LANGUAGES</h3>
+            <div class="col-12 mb-5">
+                <ul>
+                    <li>Hungarian: Native</li>
+                    <li> English: Advanced</li>
+                    <li> Spanish: Elementary</li>
+                </ul>
+            </div>
         </div>
     </div>
-
 @endsection
