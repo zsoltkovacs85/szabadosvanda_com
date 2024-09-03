d8e250249ac5caf82749dac060cd51b795854d08
Zsolt Kovacs <info@zsoltkovacs.co.uk>
2024-09-03 20:09:47 +0100

    Making the website much better by imropving images and fonts 14

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
-                <div class="d-flex justify-content-center quicksand-300">
+                <div class="d-flex justify-content-center quicksand-400">
                     <a href="{{url('/cookie-policy')}}" class="me-2">Cookie Policy</a> |
                     <a href="{{url('/privacy-statement')}}" class="ms-2">Privacy Statement</a>
                 </div>
