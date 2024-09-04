4caf864b9bb5274c67c28532c0ac8e0e27d19b8a
Zsolt Kovacs <info@zsoltkovacs.co.uk>
2024-09-04 21:44:08 +0100

    Fixing the Cruise ship - Fixing the Back URL 7

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -13,9 +13,9 @@
 
     <div class="d-flex justify-content-center">
         <div class="mySlideshow">
-            @for($i = 1; $i < 11; $i ++)
+            @for($i = 1; $i < 7; $i ++)
                 <a href="javascript:void(0)">
-                    <img src="{{asset('assets/img/slider/SL'.$i.'.png')}}">
+                    <img src="{{asset('assets/img/slider/SLM'.$i.'.png')}}">
                 </a>
             @endfor
         </div>
